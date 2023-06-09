d154a4d8c1cba99b4eda1669ffdd5593b04d86f6
Name707 <707VLAN@gmail.com>
2023-06-09 17:22:36 +0600

    Добавлено изображение

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
                     ArtSpaceLayout()
-
-                }
             }
         }
     }
 }
-
+@Preview
 @Composable
 fun ArtSpaceLayout() {
-
+    ArtSpaceImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
 
-fun artSpaceImage(
+@Composable
+fun ArtSpaceImage(
     modifier: Modifier,
-
 ) {
+    val currentImage = R.drawable.lemon_tree
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFC3FBD8)),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+        Image(
+            painter = painterResource(currentImage),
+            contentDescription = null,
+            modifier = Modifier
+                .border(10.dp, Color.White, )
+                .background(Color(0xFFFED6BC))
+        )
+    }
 
 }
