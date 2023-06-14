061dfbb0b43a83761f9e9924feaba099f4638a86
Name707 <707VLAN@gmail.com>
2023-06-14 16:37:14 +0600

    Изображение помещено на "холст" с эффектом тени.

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,8 +29,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,11 +45,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                    ArtSpaceLayout()
+                ArtSpaceLayout()
             }
         }
     }
 }
+
 @Preview
 @Composable
 fun ArtSpaceLayout() {
@@ -55,21 +71,43 @@ fun ArtSpaceImage(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFC3FBD8)),
-        horizontalAlignment = Alignment.CenterHorizontally
-
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
         Spacer(
-            modifier = Modifier.height(20.dp)
+            modifier = Modifier
+                .height(150.dp)
         )
 
-        Image(
-            painter = painterResource(currentImage),
-            contentDescription = null,
+        Column(
             modifier = Modifier
-                .border(10.dp, Color.White, )
-                .background(Color(0xFFFED6BC))
+                .shadow(
+                    elevation = 10.dp,
+                )
+                .background(Color.White)
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(50.dp)
+                    .background(color = Color.LightGray),
+                painter = painterResource(currentImage),
+                contentDescription = null,  /*stringResource(R.string.Lemonade_tree)*/
+            )
+        }
+
+        Spacer(
+            modifier = Modifier
+                .height(150.dp)
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color.Yellow)
+        ) {
+
+        }
     }
 
 }
