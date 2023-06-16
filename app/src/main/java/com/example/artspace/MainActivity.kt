369bb3a1de66a1f9cf7ec108fbfa3fde3055b66f
Name707 <707VLAN@gmail.com>
2023-06-16 17:55:34 +0600

    Доработана плашка и добавлены кнопки (с одном Row без функционала, разделены .padding-ами)

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerDefaults.containerColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +31,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 import androidx.compose.material3.Text
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +50,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun ArtSpaceLayout() {
-    ArtSpaceImage(
+    ArtSpace(
         modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
@@ -49,7 +58,7 @@ fun ArtSpaceLayout() {
 }
 
 @Composable
-fun ArtSpaceImage(
+fun ArtSpace(
     modifier: Modifier,
 ) {
     val currentImage = R.drawable.lemon_tree
@@ -63,7 +72,7 @@ fun ArtSpaceImage(
 
         Spacer(
             modifier = Modifier
-                .height(150.dp)
+                .height(100.dp)
         )
 
         Column(
@@ -87,8 +96,14 @@ fun ArtSpaceImage(
                 .height(150.dp)
         )
 
-        CanvasInfo("Lemon Tree", "Empty Author (2023)")
+        CanvasInfo("Lemon Tree", "Empty Author", 2023)
 
+        Spacer(
+            modifier = Modifier
+                .height(80.dp)
+        )
+
+        NextPrevButton("Previous", "Next")
     }
 
 }
@@ -96,21 +111,90 @@ fun ArtSpaceImage(
 @Composable
 fun CanvasInfo(
     canvasName: String,
-    authorAndDate: String,
+    author: String,
+    date: Short,
 ) {
 
     Row(
         modifier = Modifier
             .width(270.dp)
-            .height(70.dp)
+            .height(50.dp)
             .background(Color(0xFFece8ff))
-            .padding(15.dp),
+            .padding(7.dp),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically,
     ) {
 
         Text(
-            text = "$canvasName\n$authorAndDate"
+            text = "$canvasName",
+            fontSize = 30.sp,
         )
+    }
+
+    Row(
+        modifier = Modifier
+            .width(270.dp)
+            .height(35.dp)
+            .background(Color(0xFFece8ff))
+            .padding(7.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Text(
+            text = "$author ($date)",
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun NextPrevButton(
+    buttonTextPrev: String,
+    buttonTextNext: String,
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(25.dp)
+                .height(35.dp)
+                .width(120.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF5394bd),
+            )
+
+        ) {
+
+            Text(
+                text = buttonTextPrev
+            )
+
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(25.dp)
+                .height(35.dp)
+                .width(120.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF5394bd),
+            )
+        ) {
+
+            Text(
+                text = buttonTextNext
+            )
+
+        }
     }
 }
