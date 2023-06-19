38a821102bfcbbfcccc05597301dfde1a3674225
Name707 <707VLAN@gmail.com>
2023-06-19 21:23:29 +0600

    Черновой вариант работает, на конечном устройстве кнопки не помещаются на экране

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.artspace
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,9 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DrawerDefaults.containerColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,7 +27,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 import androidx.compose.material3.Text
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
@@ -61,7 +61,29 @@ fun ArtSpaceLayout() {
 fun ArtSpace(
     modifier: Modifier,
 ) {
-    val currentImage = R.drawable.lemon_tree
+    var clickResult by remember {
+        mutableStateOf(0)
+    }
+
+    if (clickResult >= 3) clickResult = 3
+    if (clickResult <= 0) clickResult = 0
+
+    val actualImage = when (clickResult) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    val canvasInfoBanner = when (actualImage) {
+        R.drawable.lemon_tree -> stringResource(R.string.Lemon_tree)
+        R.drawable.lemon_squeeze -> stringResource(R.string.Lemon_squeeze)
+        R.drawable.lemon_drink -> stringResource(R.string.Lemonade_cup)
+        R.drawable.lemon_restart -> stringResource(R.string.Lemonade_drink)
+        else -> "The End"
+    }
+
+    val parsingCanvasInfoBanner = canvasInfoBanner.split(";")
 
     Column(
         modifier = Modifier
@@ -86,8 +108,8 @@ fun ArtSpace(
                 modifier = Modifier
                     .padding(50.dp)
                     .background(color = Color.LightGray),
-                painter = painterResource(currentImage),
-                contentDescription = null,  /*stringResource(R.string.Lemonade_tree)*/
+                painter = painterResource(actualImage),
+                contentDescription = null,
             )
         }
 
@@ -96,105 +118,86 @@ fun ArtSpace(
                 .height(150.dp)
         )
 
-        CanvasInfo("Lemon Tree", "Empty Author", 2023)
+        Row(
+            modifier = Modifier
+                .width(270.dp)
+                .height(50.dp)
+                .background(Color(0xFFece8ff))
+                .padding(7.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            Text(
+                text = parsingCanvasInfoBanner[0],
+                fontSize = 30.sp,
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .width(270.dp)
+                .height(35.dp)
+                .background(Color(0xFFece8ff))
+                .padding(7.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            Text(
+                text = "${parsingCanvasInfoBanner[1]} (${parsingCanvasInfoBanner[2]})",
+                fontWeight = FontWeight.Bold,
+            )
+        }
 
         Spacer(
             modifier = Modifier
                 .height(80.dp)
         )
 
-        NextPrevButton("Previous", "Next")
-    }
-
-}
-
-@Composable
-fun CanvasInfo(
-    canvasName: String,
-    author: String,
-    date: Short,
-) {
-
-    Row(
-        modifier = Modifier
-            .width(270.dp)
-            .height(50.dp)
-            .background(Color(0xFFece8ff))
-            .padding(7.dp),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-
-        Text(
-            text = "$canvasName",
-            fontSize = 30.sp,
-        )
-    }
-
-    Row(
-        modifier = Modifier
-            .width(270.dp)
-            .height(35.dp)
-            .background(Color(0xFFece8ff))
-            .padding(7.dp),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-
-        Text(
-            text = "$author ($date)",
-            fontWeight = FontWeight.Bold,
-        )
-    }
-}
-
-@Composable
-fun NextPrevButton(
-    buttonTextPrev: String,
-    buttonTextNext: String,
-) {
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-
-        Button(
-            onClick = {},
+        Row(
             modifier = Modifier
-                .padding(25.dp)
-                .height(35.dp)
-                .width(120.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF5394bd),
-            )
-
+                .fillMaxWidth()
+                .height(100.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
+            Button(
+                onClick = { clickResult-- },
+                modifier = Modifier
+                    .padding(25.dp)
+                    .height(35.dp)
+                    .width(120.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5394bd),
+                )
 
-            Text(
-                text = buttonTextPrev
-            )
+            ) {
 
-        }
+                Text(
+                    text = "Previous",
+                )
+            }
 
-        Button(
-            onClick = {},
-            modifier = Modifier
-                .padding(25.dp)
-                .height(35.dp)
-                .width(120.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF5394bd),
-            )
-        ) {
+            Button(
+                onClick = { clickResult++ },
+                modifier = Modifier
+                    .padding(25.dp)
+                    .height(35.dp)
+                    .width(120.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5394bd),
+                )
 
-            Text(
-                text = buttonTextNext
-            )
+            ) {
+
+                Text(
+                    text = "Next",
+                )
+            }
 
         }
     }
+
 }
+
