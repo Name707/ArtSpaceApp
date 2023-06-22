794482a36aae30a594bf30e50290efcaa880436a
Name707 <707VLAN@gmail.com>
2023-06-22 19:37:40 +0600

    В качестве экспонатов добавлены реальные картины. Размеры подогнаны под размер экрана 6.1"

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
@@ -31,8 +33,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
@@ -65,21 +71,21 @@ fun ArtSpace(
         mutableStateOf(0)
     }
 
-    if (clickResult >= 3) clickResult = 3
-    if (clickResult <= 0) clickResult = 0
+    if (clickResult >= 3) clickResult = 3 else if (clickResult <= 0) clickResult = 0
+
 
     val actualImage = when (clickResult) {
-        0 -> R.drawable.lemon_tree
-        1 -> R.drawable.lemon_squeeze
-        2 -> R.drawable.lemon_drink
-        else -> R.drawable.lemon_restart
+        0 -> R.drawable.girl_with_peaches
+        1 -> R.drawable.pearl_earring
+        2 -> R.drawable.persistence_of_memory
+        else -> R.drawable.starlight_night
     }
 
     val canvasInfoBanner = when (actualImage) {
-        R.drawable.lemon_tree -> stringResource(R.string.Lemon_tree)
-        R.drawable.lemon_squeeze -> stringResource(R.string.Lemon_squeeze)
-        R.drawable.lemon_drink -> stringResource(R.string.Lemonade_cup)
-        R.drawable.lemon_restart -> stringResource(R.string.Lemonade_drink)
+        R.drawable.girl_with_peaches -> stringResource(R.string.girl_with_peaches)
+        R.drawable.pearl_earring -> stringResource(R.string.pearl_earring)
+        R.drawable.persistence_of_memory -> stringResource(R.string.persistence_of_memory)
+        R.drawable.starlight_night -> stringResource(R.string.starlight_night)
         else -> "The End"
     }
 
@@ -88,13 +94,14 @@ fun ArtSpace(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(Color.White)
+            .wrapContentWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
         Spacer(
             modifier = Modifier
-                .height(100.dp)
+                .height(80.dp)
         )
 
         Column(
@@ -105,23 +112,25 @@ fun ArtSpace(
                 .background(Color.White)
         ) {
             Image(
+                bitmap = ImageBitmap.imageResource(actualImage),
                 modifier = Modifier
-                    .padding(50.dp)
-                    .background(color = Color.LightGray),
-                painter = painterResource(actualImage),
+                    .height(300.dp)
+                    .widthIn(280.dp, 325.dp)
+                    .padding(25.dp),
+                contentScale = ContentScale.Fit,
                 contentDescription = null,
             )
         }
 
         Spacer(
             modifier = Modifier
-                .height(150.dp)
+                .height(100.dp)
         )
 
         Row(
             modifier = Modifier
-                .width(270.dp)
-                .height(50.dp)
+                .width(350.dp)
+                .height(80.dp)
                 .background(Color(0xFFece8ff))
                 .padding(7.dp),
             horizontalArrangement = Arrangement.Start,
@@ -130,14 +139,15 @@ fun ArtSpace(
 
             Text(
                 text = parsingCanvasInfoBanner[0],
-                fontSize = 30.sp,
+                overflow = TextOverflow.Visible,
+                fontSize = 25.sp,
             )
         }
 
         Row(
             modifier = Modifier
-                .width(270.dp)
-                .height(35.dp)
+                .width(350.dp)
+                .height(40.dp)
                 .background(Color(0xFFece8ff))
                 .padding(7.dp),
             horizontalArrangement = Arrangement.Start,
